650fbe2cc318092f64d6112dc7122ffcef2e5a6f
Bradley Claus <xinnx009@gmail.com>
2017-01-31 19:18:28 -0500

    eclipse class path

---
+++ src/main/java/searchui/MainWindow.java
@@ -0,0 +1,11 @@
+package searchui;
+
+/**
+ * Created by bclaus on 1/31/17.
+ */
+
+public class MainWindow {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
